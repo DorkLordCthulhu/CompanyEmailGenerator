858bafe5e54ba7d8b008b0521b68ca710c6de546
Britta <50714303+DorkLordCthulhu@users.noreply.github.com>
2020-07-10 11:55:21 -0400

    Create EmailApp.java

---
+++ EmailApp.java
@@ -0,0 +1,9 @@
+package emailapp;
+
+public class EmailApp {
+
+    public static void main(String[] args) {
+        emailapp.Email em1 = new emailapp.Email("John", "Smith");
+        em1.createAccount();
+    }
+}
